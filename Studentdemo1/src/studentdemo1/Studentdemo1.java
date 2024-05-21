5becdd65e44899a72340feabeb3afb2e6d020bb6
Nguyen Huong Giang <nguyenhuonggiang9224@gmail.com>
2024-05-21 15:39:30 +0700

    1st commit

---
+++ Studentdemo1/src/studentdemo1/Studentdemo1.java
@@ -0,0 +1,15 @@
+
+package studentdemo1;
+
+
+public class Studentdemo1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFStudent jfs = new JFStudent ();
+        jfs.show();
+        
+    }
+    
+}
